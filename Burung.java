9f15972c840fb3bda8b9ba776f388b3672f37438
21chelika <85401286+21chelika@users.noreply.github.com>
2021-12-06 21:19:37 +0700

    Create Burung.java

---
+++ Burung.java
@@ -0,0 +1,18 @@
+Public class Burung ekstends hewan{
+String type = "Burung";
+
+Burung(String nama){
+Super (nama);
+
+}
+
+@override
+Void display(){
+Super.display();
+System.out.println("type\t : "+this.type);
+}
+
+Void.showoff(){
+Sytstem.out.println("krik krik krik";
+
+}
