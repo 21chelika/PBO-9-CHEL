70756becafd18b95caaadd8d2b06e18f14e9bbb7
21chelika <85401286+21chelika@users.noreply.github.com>
2021-12-06 21:11:26 +0700

    Create Polymorphism.java

---
+++ Polymorphism.java
@@ -0,0 +1,12 @@
+Public Class Polymosphism {
+Public static void main (string [] args){
+
+hewan hewan1 = new kucing ("pio");
+hewan.display();
+
+Anjing hewan2 = new Anjing ("popi");
+hewan.display();
+
+Burung hewan4 = new burung ("popo");
+hewan4.display();
+hewan4.showoff();
